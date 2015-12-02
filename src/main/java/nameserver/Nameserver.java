dd35dc09cd1f63a4206e0d85745bbe0dba58e075
Salocin <e0929245@student.tuwien.ac.at>
2015-12-02 17:46:55 +0100

    registerNameserver, registerUser implemented

--- src/main/java/nameserver/Nameserver.java
+++ src/main/java/nameserver/Nameserver.java
@@ -11,8 +11,9 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.RemoteObject;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.List;
+import java.util.*;
 
+import cli.Command;
 import cli.Shell;
 import nameserver.exceptions.AlreadyRegisteredException;
 import nameserver.exceptions.InvalidDomainException;
@@ -32,7 +33,10 @@ public class Nameserver implements INameserverCli, INameserver, Runnable {
 	private Shell shell;
 
 	public static Registry registry;
-	private List<Nameserver> children;
+	//Key: Domain, Value: Remote Object (INameserver)
+	private HashMap<String, INameserver> children;
+	//Key: username, Value: address
+	private HashMap<String, String> users;
 
 	/**
 	 * @param componentName
@@ -50,14 +54,19 @@ public class Nameserver implements INameserverCli, INameserver, Runnable {
 		this.config = config;
 		this.userRequestStream = userRequestStream;
 		this.userResponseStream = userResponseStream;
+		this.children = new HashMap<>();
+		this.users = new HashMap<>();
 
 		shell = new Shell(this.componentName, this.userRequestStream, this.userResponseStream);
 		shell.register(this);
-
+		new Thread(shell).start();
 	}
 
 	@Override
 	public void run() {
+
+		String host = config.getString("registry.host");
+		Integer port = config.getInt("registry.port");
 
 		//root nameserver is the only nameserver that does not have the domain property
 		if(!config.listKeys().contains("domain")){
@@ -66,8 +75,8 @@ public class Nameserver implements INameserverCli, INameserver, Runnable {
 			try {
 				registry = LocateRegistry.createRegistry(config.getInt("registry.port"));
 
-				//create the remote object. INameserver is extending Remote
-				INameserver remoteObject = (INameserver) RemoteObject.toStub(this);
+				//Exports the remote object to make it available to receive incoming calls, using the particular supplied port.
+				INameserver remoteObject = (INameserver) UnicastRemoteObject.exportObject(this,0);
 
 				//bind the remote object to the registry so that it can be called from other nameservers
 				//binding name is specified in .properties xml
@@ -83,17 +92,15 @@ public class Nameserver implements INameserverCli, INameserver, Runnable {
 			}
 		}else{
 			//register the nameserver at registry
-			String host = config.getString("registry.host");
-			Integer port = config.getInt("registry.port");
 			String domain = config.getString("domain");
-
 			try {
 				Registry registry = LocateRegistry.getRegistry(host,port);
 				//lookup the remote object which is bound to registry
 				INameserver root = (INameserver) registry.lookup(config.getString("root_id"));
 
-				//create remote object of this nameserver class and pass it to the root nameserver
-				root.registerNameserver(domain, root, root);
+				//create remote object of this nameserver class and register this nameserver in the network
+				INameserver remoteObject = (INameserver) UnicastRemoteObject.exportObject(this, 0);
+				root.registerNameserver(domain, remoteObject, remoteObject);
 
 			} catch (RemoteException e) {
 				e.printStackTrace();
@@ -107,32 +114,100 @@ public class Nameserver implements INameserverCli, INameserver, Runnable {
 		}
 	}
 
+	@Command
 	@Override
 	public String nameservers() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		SortedSet<String> keys = new TreeSet<String>(children.keySet());
+		String nameservers = "";
+		int i = 1;
+		for (String key : keys) {
+			nameservers += i + ". " + key + "\n";
+			i++;
+		}
+		return nameservers;
 	}
 
+	/**
+	 * Prints out all users that are registered under this domain
+	 * @throws IOException
+     */
+	@Command
 	@Override
 	public String addresses() throws IOException {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public String exit() throws IOException {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
+	/**
+	 * This method registers a new nameserver in the network at specific location which is stated by the domain
+	 * @param domain the location of the new nameserver
+	 * @param nameserver
+	 * @param nameserverForChatserver
+	 * @throws RemoteException
+	 * @throws AlreadyRegisteredException is thrown when the domain is already registered and saved as a child in the hashmap
+	 * @throws InvalidDomainException is thrown when a nameserver tries to register with one or more subdomains even though the highest level domain was not yet registered
+     */
 	@Override
 	public void registerNameserver(String domain, INameserver nameserver, INameserverForChatserver nameserverForChatserver) throws RemoteException, AlreadyRegisteredException, InvalidDomainException {
+		//split domain
+		String[] domains = domain.split("\\.");
+
+		if(domains.length == 1){
+			//in this case the we are on the deepest level -> if already in children than the server is already registered
+			if(children.get(domain)!=null){
+				throw new AlreadyRegisteredException("The domain " + domain + " is already registered.");
+			}else {
+				userResponseStream.println("Successfully registered " + domain);
+				children.put(domain,nameserver);
+			}
+		}else{
+			String highestDomain = domains[domains.length-1];
+			String subDomain = domain.substring(0,domain.length()-1-highestDomain.length());
+			//is the highestDomain already in children?
+			INameserver nameserverOfDomain = children.get(highestDomain);
+			if(children.get(highestDomain)!=null){
+				nameserverOfDomain.registerNameserver(subDomain, nameserver, nameserverForChatserver);
+			}else{
+				throw new InvalidDomainException("To register " + domain + " you have to register " + highestDomain + " first.");
+			}
+		}
+
 
 	}
 
+
+	/**
+	 * Registers a user. the username contains the domain it wants to be registered to. domains are splitted by "."
+	 * @param username
+	 * @param address
+	 * @throws RemoteException
+	 * @throws AlreadyRegisteredException
+	 * @throws InvalidDomainException
+     */
+	@Command
 	@Override
 	public void registerUser(String username, String address) throws RemoteException, AlreadyRegisteredException, InvalidDomainException {
-
+		String[] splittedUsername = username.split("\\.");
+		if(splittedUsername.length == 1){
+			if(users.containsKey(username))
+				throw new AlreadyRegisteredException("The user " + username + " is already registered");
+			else {
+				users.put(username,address);
+			}
+		}else {
+			//In this case the request has to be delegated to next nameserver
+			String domain = splittedUsername[splittedUsername.length-1];
+			if(children.containsKey(domain)){
+				children.get(domain).registerUser(username.substring(0,username.length()-domain.length()-1),address);
+			}else
+				throw new InvalidDomainException("The domain "+ domain + " is not registered!");
+		}
 	}
 
 	@Override
